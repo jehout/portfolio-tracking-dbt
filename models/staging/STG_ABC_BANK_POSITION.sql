7ddd8f2d4d0b9cfb659535de728901b08dec4b3a
jerome.marx@protonmail.com <jerome.marx@protonmail.com>
2023-09-04 14:34:43 +0000

    added STG_ABC_BANK_POSITION changed dbt_project.yml: refactored comments

---
+++ models/staging/STG_ABC_BANK_POSITION.sql
@@ -0,0 +1,11 @@
+select 
+      ACCOUNTID         as ACCOUNT_CODE     -- TEXT
+    , SYMBOL            as SECURITY_CODE    -- TEXT
+    , DESCRIPTION       as SECURITY_NAME    -- TEXT
+    , EXCHANGE          as EXCHANGE_CODE    -- TEXT
+    , REPORT_DATE       as REPORT_DATE      -- DATE
+    , QUANTITY          as QUANTITY         -- NUMBER
+    , COST_BASE         as COST_BASE        -- NUMBER
+    , POSITION_VALUE    as POSITION_VALUE   -- NUMBER
+    , CURRENCY          as CURRENCY_CODE    -- TEXT
+from {{source('abc_bank', 'ABC_BANK_POSITION')}}
