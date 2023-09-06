49819a52547f5f7b6de970454f502627ab57a8e2
jerome.marx@protonmail.com <jerome.marx@protonmail.com>
2023-09-06 10:25:00 +0000

    elaborated REF_POSITION_ABC_BANK to only read the current records from the SNSH_ABC_BANK_POSITION

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,4 +1,10 @@
+WITH current_from_snapshot as (
+    select *
+    from {{ref('SNSH_ABC_BANK_POSITION')}}
+    where DBT_VALID_TO is null
+)
+
 select *
   , POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT
   , ROUND(UNREALIZED_PROFIT / COST_BASE, 5) as UNREALIZED_PROFIT_PCT
-from {{ref('STG_ABC_BANK_POSITION')}}
+from current_from_snapshot  -- we only want to have the active/current records
