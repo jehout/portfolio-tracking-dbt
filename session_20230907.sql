780ae7cddfc7f9a5ce484dc042800057c5a919d0
jerome.marx@protonmail.com <jerome.marx@protonmail.com>
2023-09-07 15:14:34 +0000

    SNSH_ABC_BANK_SECURITY_INFO: init version

--- session_20230907.sql
+++ session_20230907.sql
@@ -6,4 +6,5 @@
       AND TABLE_NAME = 'ABC_BANK_SECURITY_INFO'
     ORDER BY ORDINAL_POSITION;                       -- the order of the columns in the table
 
-select * from {{ref('STG_ABC_BANK_SECURITY_INFO')}};
+--select * from {{source('seeds', 'ABC_Bank_SECURITY_INFO')}};
+select SECURITY_CODE, SECURITY_HDIFF from {{ref('STG_ABC_BANK_SECURITY_INFO')}};
