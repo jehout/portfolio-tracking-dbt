fa3816cbce348aabc6460936302da8a9dc6e951c
jerome.marx@protonmail.com <jerome.marx@protonmail.com>
2023-09-07 10:08:20 +0000

    ephemeral STG_ABC_BANK_SECURITY_INFO created ... ... with default row and hash columns ...on source seeds.ABC_Bank_SECURITY_INFO

---
+++ session_20230907.sql
@@ -0,0 +1,9 @@
+-- generating Adapter
+    SELECT
+      ', ' || COLUMN_NAME || ' as '|| COLUMN_NAME  || ' -- ' || DATA_TYPE as SQL_TEXT
+    FROM PORTFOLIO_TRACKING.INFORMATION_SCHEMA.COLUMNS  -- adapt to your DB
+    WHERE TABLE_SCHEMA = 'JEH_SEED_DATA'
+      AND TABLE_NAME = 'ABC_BANK_SECURITY_INFO'
+    ORDER BY ORDINAL_POSITION;                       -- the order of the columns in the table
+
+select * from {{ref('STG_ABC_BANK_SECURITY_INFO')}};
