5afdbb54bd776454f81007cd2807d8bb5c15d851
jerome.marx@protonmail.com <jerome.marx@protonmail.com>
2023-09-12 09:25:08 +0000

    STG_ABC_BANK_POSITION+ : fixing the HKEY, HDIFF using dbt-utils/generate-surrogate_key

--- models/staging/STG_ABC_BANK_POSITION.sql
+++ models/staging/STG_ABC_BANK_POSITION.sql
@@ -21,10 +21,13 @@ src_data as (
 
 hashed as (
     SELECT
-          concat_ws('|', ACCOUNT_CODE, SECURITY_CODE) as POSITION_HKEY
-        , concat_ws('|', ACCOUNT_CODE, SECURITY_CODE,
-                SECURITY_NAME, EXCHANGE_CODE, REPORT_DATE,
-                QUANTITY, COST_BASE, POSITION_VALUE, CURRENCY_CODE ) as POSITION_HDIFF
+        {{ dbt_utils.generate_surrogate_key(['ACCOUNT_CODE', 'SECURITY_CODE']) 
+        }} as POSITION_HKEY
+        , {{ dbt_utils.generate_surrogate_key([
+            'ACCOUNT_CODE', 'SECURITY_CODE',
+            'SECURITY_NAME', 'EXCHANGE_CODE', 'REPORT_DATE',
+            'QUANTITY', 'COST_BASE', 'POSITION_VALUE', 'CURRENCY_CODE']) 
+        }} as POSITION_HDIFF
         , *
         , '{{ run_started_at }}'::timestamp as LOAD_TS_UTC
     FROM src_data
--- session_20230907.sql
+++ session_20230907.sql
@@ -30,3 +30,11 @@ select * from {{ ref('STG_EXCHANGE') }};
 select *, year(report_date), month(report_date), day(report_date) 
 , year(dateadd(year, 2000, report_date))--, month(report_date), day(report_date) 
 from {{ source('abc_bank', 'ABC_BANK_POSITION') }};
+
+-- calculating hashes for HKEY, HDIFF of STG_ABC_BANK_POSITION
+--- drop snsh 
+select * from {{ ref('SNSH_ABC_BANK_POSITION') }}; -- for checking if the HKEY and HREF are fixed
+--- drop table {{ ref('SNSH_ABC_BANK_POSITION') }}; -- can not drop in dbt worksheet, dbt adds a 'limit 500' to the DROP statement
+--- rebuild the snapshot table: dbt snapshot -s SNSH_ABC_BANK_POSITION
+select * from {{ ref('REF_POSITION_ABC_BANK') }};
+select * from {{ ref('FACT_POSITION') }};
