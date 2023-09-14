09a30b73a6d40eb875bb08641f9e0d723e558aa7
jerome.marx@protonmail.com <jerome.marx@protonmail.com>
2023-09-14 11:15:09 +0000

    macro to_21st_century_date: alternatieve test

--- tests/macros/to_21st_century_date.sql
+++ tests/macros/to_21st_century_date.sql
@@ -5,6 +5,12 @@ input_data as (
     select 1 as id, '2001-01-01'::date as input, '2001-01-01'::date as expected_result
     UNION
     select 2 as id, '0023-01-01'::date as input, '2023-01-01'::date as expected_result
+),
+macro_data as (
+    select id, {{ to_21st_century_date('input')}} as actual_result 
+    from input_data
 )
-select *, {{ to_21st_century_date('input')}} as actual_result from input_data
+select *, actual_result
+from input_data
+join macro_data on input_data.id = macro_data.id
 where expected_result != actual_result
